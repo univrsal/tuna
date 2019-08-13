00fb3166a7c89ed8b449121e3507a584ec728b5e
univrsal <univrsal@protonmail.ch>
2019-08-13 19:04:40 +0200

    readded placeholder credentials file

---
+++ src/util/creds.hpp
@@ -0,0 +1,10 @@
+/**
+ * This file is part of tuna
+ * which is licensed under the GPL v2.0
+ * See LICENSE or http://www.gnu.org/licenses
+ * github.com/univrsal/tuna
+ */
+#pragma once
+
+#define SPOTIFY_CLIENT_ID	"{PLACEHOLDER}"
+#define SPOTIFY_CLIENT_SECRET	"{PLACEHOLDER}"
