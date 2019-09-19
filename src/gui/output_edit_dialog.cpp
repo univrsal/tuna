0e377e7a8510d6d9c2502dd732fd1f530cbeb8e6
univrsal <univrsal@protonmail.ch>
2019-09-19 21:29:54 +0200

    fixed license header

--- src/gui/output_edit_dialog.cpp
+++ src/gui/output_edit_dialog.cpp
@@ -1,18 +1,21 @@
-/* output_edit_dialog.cpp created on 2019.9.18
+/*************************************************************************
+ * This file is part of tuna
+ * github.con/univrsal/tuna
+ * Copyright 2019 univrsal <universailp@web.de>.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation, version 2 of the License.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * github.com/univrsal/
- *
- */
+ *************************************************************************/
+
 #include "output_edit_dialog.hpp"
 #include "ui_output_edit_dialog.h"
 #include "tuna_gui.hpp"
