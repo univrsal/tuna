a8058e85a5723030e5d14ed3066dbbf0730f6a78
univrsal <uni@vrsal.de>
2021-04-11 00:56:47 +0200

    use clang-format file

--- format.sh
+++ format.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 grep -rl 'Copyright 2019' ./src | xargs sed -i 's/Copyright 2019/Copyright 2020/g'
-find ./src -iname *.h* -o -iname *.c* | xargs clang-format -style=WebKit -i -verbose
+find ./src -iname *.h* -o -iname *.c* | xargs clang-format -style=File -i -verbose
