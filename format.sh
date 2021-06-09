d15f42fb15d4e6014bd553f8995d7037a28f7e45
univrsal <uni@vrsal.de>
2021-06-09 20:48:43 +0200

    update format script

--- format.sh
+++ format.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-grep -rl 'Copyright 2019' ./src | xargs sed -i 's/Copyright 2019/Copyright 2020/g'
+#grep -rl 'Copyright 2020' ./src | xargs sed -i 's/Copyright 2020/Copyright 2021/g'
+#grep -rl 'uni@vrsal.cf' ./src | xargs sed -i 's/uni@vrsal.cf/uni@vrsal.de/g'
 find ./src -iname *.h* -o -iname *.c* | xargs clang-format -style=File -i -verbose
