1d32bd942493f7d36a7b498323daced42b41a1a6
univrsal <uni@vrsal.de>
2021-06-09 20:48:20 +0200

    Update year and email

--- src/query/spotify_source.cpp
+++ src/query/spotify_source.cpp
@@ -1,7 +1,7 @@
 /*************************************************************************
  * This file is part of tuna
  * github.com/univrsal/tuna
- * Copyright 2020 univrsal <universailp@web.de>.
+ * Copyright 2021 univrsal <uni@vrsal.de>.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
