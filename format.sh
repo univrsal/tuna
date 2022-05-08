13600a776dbab2003cf5d75893bef878be7754ae
univrsal <uni@vrsal.xyz>
2022-05-08 18:09:57 +0200

    Misc: Update format script

--- format.sh
+++ format.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #grep -rl 'Copyright 2021' ./src | xargs sed -i 's/Copyright 2021/Copyright 2022/g'
-#grep -rl 'uni@vrsal.de' ./src | xargs sed -i 's/uni@vrsal.cf/uni@vrsal.xyz/g'
+#grep -rl 'uni@vrsal.de' ./src | xargs sed -i 's/uni@vrsal.de/uni@vrsal.xyz/g'
 find ./src -iname *.h* -o -iname *.c* | xargs clang-format -style=File -i -verbose
