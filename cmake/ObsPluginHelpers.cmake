d0d15803a3521a255c50a6de55591540d0dab428
univrsal <uni@vrsal.xyz>
2022-08-12 22:48:43 +0200

    CI: Maybe fix macOS cmake issue?

--- cmake/ObsPluginHelpers.cmake
+++ cmake/ObsPluginHelpers.cmake
@@ -466,7 +466,7 @@ if(OS_MACOS)
         target_sources(${target} PRIVATE ${_DATA_FILE})
         set_source_files_properties(${_DATA_FILE} PROPERTIES MACOSX_PACKAGE_LOCATION
                                                              Resources/${_RELATIVE_PATH})
-        string(REPLACE "\\" "\\\\" _GROUP_NAME ${_RELATIVE_PATH})
+        string(REPLACE "\\" "\\\\" ${_GROUP_NAME} ${_RELATIVE_PATH})
         source_group("Resources\\${_GROUP_NAME}" FILES ${_DATA_FILE})
       endforeach()
     endif()
