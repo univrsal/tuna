ca3bfef085489669538eb6dc5fdb542c762ba956
univrsal <uni@vrsal.xyz>
2022-10-22 00:59:25 +0200

    Util: Output debug as info in debug builds
    The debug log level is apparently not visible on the log by default

--- src/util/utility.hpp
+++ src/util/utility.hpp
@@ -28,7 +28,11 @@
 
 #define write_log(log_level, format, ...) blog(log_level, "[tuna] " format, ##__VA_ARGS__)
 
-#define bdebug(format, ...) write_log(LOG_DEBUG, format, ##__VA_ARGS__)
+#if defined(_DEBUG)
+#    define bdebug(format, ...) write_log(LOG_INFO, format, ##__VA_ARGS__) // apparently debug level isn't logged by default, so we just use info level for debug builds
+#else
+#    define bdebug(format, ...) write_log(LOG_DEBUG, format, ##__VA_ARGS__)
+#endif
 #define binfo(format, ...) write_log(LOG_INFO, format, ##__VA_ARGS__)
 #define bwarn(format, ...) write_log(LOG_WARNING, format, ##__VA_ARGS__)
 #define berr(format, ...) write_log(LOG_ERROR, format, ##__VA_ARGS__)
