7288de0f82dc370cd02e6167e7e1aaf66f2df953
univrsal <uni@vrsal.xyz>
2023-02-06 18:05:06 +0100

    Misc: Update headers

--- src/gui/scrolltext.cpp
+++ src/gui/scrolltext.cpp
@@ -1,7 +1,7 @@
 ﻿/*************************************************************************
  * This file is part of tuna
  * git.vrsal.xyz/alex/tuna
- * Copyright 2022 univrsal <uni@vrsal.xyz>.
+ * Copyright 2023 univrsal <uni@vrsal.xyz>.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/query/mpris_source.cpp
+++ src/query/mpris_source.cpp
@@ -1,6 +1,6 @@
 /**
  ** This file is part of the tuna project.
- ** Copyright 2022 univrsal <uni@vrsal.xyz>.
+ ** Copyright 2023 univrsal <uni@vrsal.xyz>.
  **
  ** This program is free software: you can redistribute it and/or modify
  ** it under the terms of the GNU General Public License as published by
