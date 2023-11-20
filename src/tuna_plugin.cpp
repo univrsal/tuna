af35a99df54a3c9e2b7f0369075c70a5468e38eb
univrsal <uni@vrsal.xyz>
2023-11-20 22:33:39 +0100

    Plugin: Undo thread init
    This doesn't play well with Qt. I'm pretty sure I've done this mistake
    before.

--- src/tuna_plugin.cpp
+++ src/tuna_plugin.cpp
@@ -96,24 +96,21 @@ void tuna_save_cb(obs_data_t* save_data, bool saving, void*)
 
 bool obs_module_load()
 {
-    binfo("Loading v%s-%s-%s (build time %s). Qt version: compile-time: %s, run-time: %s. libobs: compile-time: %i.%i.%i, run-time: %s",
-        TUNA_VERSION, GIT_BRANCH, GIT_COMMIT_HASH, BUILD_TIME, QT_VERSION_STR, qVersion(), LIBOBS_API_MAJOR_VER, LIBOBS_API_MINOR_VER, LIBOBS_API_PATCH_VER, obs_get_version_string());
-    std::thread t([] {
-        config::init();
-        register_gui();
-        format::init();
-        music_sources::init();
-        config::load();
-        obs_sources::register_progress();
-        obs_frontend_add_save_callback(&tuna_save_cb, nullptr);
+    binfo("Loading v%s-%s-%s (build time %s). Qt version: compile-time: %s, run-time: %s. libobs: compile-time: %s, run-time: %s",
+        TUNA_VERSION, GIT_BRANCH, GIT_COMMIT_HASH, BUILD_TIME, QT_VERSION_STR, qVersion(), OBS_VERSION_CANONICAL, obs_get_version_string());
+    config::init();
+    register_gui();
+    music_sources::init();
+    config::load();
+    format::init();
+    obs_sources::register_progress();
+    obs_frontend_add_save_callback(&tuna_save_cb, nullptr);
 
-        obs_frontend_add_event_callback([](enum obs_frontend_event event, void*) {
-            if (event == OBS_FRONTEND_EVENT_EXIT)
-                tuna_thread::stop();
-        },
-            nullptr);
-    });
-    t.detach();
+    obs_frontend_add_event_callback([](enum obs_frontend_event event, void*) {
+        if (event == OBS_FRONTEND_EVENT_EXIT)
+            tuna_thread::stop();
+    },
+        nullptr);
     return true;
 }
 
